3c1879fb96b38135c4bf9c2b5a0b5fe9c481d034
volodiapg <volodia.parolguarino@gmail.com>
2021-03-26 11:09:51 +0100

    Add forLoop test that fails because for loop is not yet implemented

---
+++ test/good/ForLoop.java
@@ -0,0 +1,15 @@
+class ForLoop {
+    public static void main(String[] args) {
+        System.out.println(new DoLoop().loop());
+    }
+}
+
+class DoLoop {
+    public int loop() {
+        int ii;
+        for (ii = 0; ii < 10; ++ii) {
+            System.out.println(ii);
+        }
+        return 0;
+    }
+}
